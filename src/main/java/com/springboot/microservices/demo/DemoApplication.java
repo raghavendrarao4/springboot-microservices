87acff62dbd613cd6f1cd5345201e35196e84696
Tarun Pal Singh <singhtarunpal@gmail.com>
2021-10-29 10:27:17 +0530

    Fixed /currency-exchange API and added /currency-exchange with quantity

--- src/main/java/com/springboot/microservices/demo/DemoApplication.java
+++ src/main/java/com/springboot/microservices/demo/DemoApplication.java
@@ -2,9 +2,12 @@ package com.springboot.microservices.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class DemoApplication {
+@ComponentScan(basePackages={"com..springboot"})
+public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
---
+++ src/main/java/com/springboot/serviceImpl/ExchangeServiceImpl.java
@@ -0,0 +1,38 @@
+package com.springboot.serviceImpl;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+import com.springboot.model.ExchangeValue;
+import com.springboot.service.ExchangeService;
+
+@Service
+@PropertySource("classpath:application.properties")
+public class ExchangeServiceImpl implements ExchangeService {
+	
+	//@Autowired
+	//private ExchangeValueRepository repository;
+	
+	@Autowired
+	private Environment environment;
+	
+	public ExchangeValue retrieveExchangeValue( String from,  String to,  Integer quantity) {
+		
+		ExchangeValue exchangeValue = new ExchangeValue(1l,from,to,new BigDecimal(environment.getProperty("conversionFactor1")),quantity); 
+		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		exchangeValue.setConvertedValue(exchangeValue.getConversionMultiple().multiply(new BigDecimal(quantity)));
+		return exchangeValue;		
+	}
+	
+	public ExchangeValue retrieveExchangeValue( String from,  String to) {
+		ExchangeValue exchangeValue = new ExchangeValue(1l,from,to,new BigDecimal(environment.getProperty("conversionFactor1"))); 
+		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		exchangeValue.setConvertedValue(exchangeValue.getConversionMultiple().multiply(new BigDecimal(1)));
+		return exchangeValue;		
+	}
+}
