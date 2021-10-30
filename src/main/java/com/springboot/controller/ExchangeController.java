d993ac3dc7ddcdd1fa900fef663197d1212ec21d
Tarun Pal Singh <singhtarunpal@gmail.com>
2021-10-30 12:37:14 +0530

    Added Logging to the project

--- src/main/java/com/springboot/controller/ExchangeController.java
+++ src/main/java/com/springboot/controller/ExchangeController.java
@@ -1,5 +1,7 @@
 package com.springboot.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,16 +18,18 @@ public class ExchangeController {
 	@Autowired
 	private ExchangeService exchangeService;
 	
+	final static Logger LOGGER = LogManager.getLogger(ExchangeController.class);
+	
 	@GetMapping("/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-		
+		LOGGER.info("*****ExchangeController.retrieveExchangeValue with from to");
 		ExchangeValue exchangeValue = exchangeService.retrieveExchangeValue(from, to);		
 		return exchangeValue;
 	}
 
 	@GetMapping("/from/{from}/to/{to}/quantity/{quantity}")
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to, @PathVariable Integer quantity) {
-		
+		LOGGER.info("*****ExchangeController.retrieveExchangeValue with from to with quantity");
 		ExchangeValue exchangeValue = exchangeService.retrieveExchangeValue(from, to, quantity);		
 		return exchangeValue;
 	}
--- src/main/java/com/springboot/serviceImpl/ExchangeServiceImpl.java
+++ src/main/java/com/springboot/serviceImpl/ExchangeServiceImpl.java
@@ -2,8 +2,9 @@ package com.springboot.serviceImpl;
 
 import java.math.BigDecimal;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
@@ -15,14 +16,13 @@ import com.springboot.service.ExchangeService;
 @PropertySource("classpath:application.properties")
 public class ExchangeServiceImpl implements ExchangeService {
 	
-	//@Autowired
-	//private ExchangeValueRepository repository;
+	final static Logger LOGGER = LogManager.getLogger(ExchangeServiceImpl.class);
 	
 	@Autowired
 	private Environment environment;
 	
 	public ExchangeValue retrieveExchangeValue( String from,  String to,  Integer quantity) {
-		
+		LOGGER.info("*****ExchangeServiceImpl.retrieveExchangeValue with from:{}  to:{} with quantity{}", from, to, quantity);
 		ExchangeValue exchangeValue = new ExchangeValue(1l,from,to,new BigDecimal(environment.getProperty("conversionFactor1")),quantity); 
 		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
 		exchangeValue.setConvertedValue(exchangeValue.getConversionMultiple().multiply(new BigDecimal(quantity)));
@@ -30,6 +30,7 @@ public class ExchangeServiceImpl implements ExchangeService {
 	}
 	
 	public ExchangeValue retrieveExchangeValue( String from,  String to) {
+		LOGGER.info("*****ExchangeServiceImpl.retrieveExchangeValue with from:{}  to:{} ", from, to);
 		ExchangeValue exchangeValue = new ExchangeValue(1l,from,to,new BigDecimal(environment.getProperty("conversionFactor1"))); 
 		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
 		exchangeValue.setConvertedValue(exchangeValue.getConversionMultiple().multiply(new BigDecimal(1)));
